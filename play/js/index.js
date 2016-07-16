f3d55653f830e9c35df839af5a8c35946ebc5acc
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2016-07-17 05:40:00 +0900

    再生のみのサンプルを追加

---
+++ play/js/index.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    var
+        elAudio, elButton, ctx, mediaElementSource, isSP, isPlaying;
+
+    elAudio  = document.getElementById('audio');
+    elButton = document.getElementById('button');
+
+    // コンテキストを生成
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    ctx = new AudioContext();
+
+    // 音源を表すAudioNodeを生成
+    mediaElementSource = ctx.createMediaElementSource(elAudio);
+
+    // 音源を表すAudioNodeを、最終出力を表すAudioNodeに接続
+    mediaElementSource.connect(ctx.destination);
+
+    // DOMへのイベント登録
+    elButton.addEventListener(isSP ? 'touchstart' : 'click', function() {
+        elAudio[!isPlaying ? 'play' : 'pause']();
+        isPlaying = !isPlaying;
+    });
+})();
